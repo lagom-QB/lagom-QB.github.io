20c2bd6cef3dfa73a829e9d6db66912393f70e03
Tebid Quinsy <28558929+lagom-QB@users.noreply.github.com>
2022-08-01 11:49:41 +0200

    Update AboutLanguages.tsx

--- components/body/AboutLanguages.tsx
+++ components/body/AboutLanguages.tsx
@@ -58,7 +58,6 @@ function TechSkills() {
   return (
     <div className={styles.container}>
       <div className={styles.ggr}>
-        <!--<div className={styles.blueGreen} />-->
         <motion.h2
           animate={{
             color: [

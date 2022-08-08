e2e0bc2ae8fc2a744ffbb3da08ef8f4aeb471ed9
Tebid Quinsy <28558929+lagom-QB@users.noreply.github.com>
2022-08-08 04:55:41 +0200

    Update AboutBlurb.tsx

--- components/body/AboutBlurb.tsx
+++ components/body/AboutBlurb.tsx
@@ -32,10 +32,10 @@ function AboutBlurb() {
                         <br />
                         Out of Data Science, I enjoy:
                     <ul>
-                        <li><div className={styles.yellowRed}/>Walks 🥾 📸</li>
-                        <li><div className={styles.yellowRed}/>Novels 📖 and Movies 🎞️</li>
-                        <li><div className={styles.yellowRed}/>Drawing 🖋️</li>
-                        <li><div className={styles.yellowRed}/>Novelties 🎊</li>
+                        <li><div className={styles.disk}/>Walks 🥾 📸</li>
+                        <li><div className={styles.disk}/>Novels 📖 and Movies 🎞️</li>
+                        <li><div className={styles.disk}/>Drawing 🖋️</li>
+                        <li><div className={styles.disk}/>Novelties 🎊</li>
                     </ul>
                     <br/>
                         You can always 📫 | 📜 to work on a project or just for a lovely chat ☕️.
